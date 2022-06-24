a5f6af75001976ed397bb38cbfc48655559d703b
wooseong <dominance5995@gmail.com>
2022-06-24 17:26:25 +0900

    Fix port

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -5,10 +5,10 @@ CURRENT_PORT=$(cat /home/ec2-user/service_url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
 # Toggle port Number
-if [ ${CURRENT_PORT} -eq 8083 ]; then
-    TARGET_PORT=8084
-elif [ ${CURRENT_PORT} -eq 8084 ]; then
-    TARGET_PORT=8083
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+    TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
+    TARGET_PORT=8081
 else
     echo "> No WAS is connected to nginx"
     exit 1
